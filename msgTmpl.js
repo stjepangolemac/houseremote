14bb873e926656564b68bca0adf43e7bfdb15d6e
Stjepan Golemac <stjepan@(none)>
2016-05-29 20:02:50 +0200

    Changed string to int in tmpl

--- msgTmpl.js
+++ msgTmpl.js
@@ -11,5 +11,5 @@ var msg = {
     enabled : 'true';
   },
   device: 'garaza1',      // for trigger, can be undefined
-  counter: '125323';      // anti replay attack
+  counter: 125323;      // anti replay attack
 }
